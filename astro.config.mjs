65dd2c7b9df0bd6e0a8584a58187658ccbfe1bb1
Sting Alleman <stingalleman@icloud.com>
2022-11-04 17:30:52 +0100

    bye react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind()],
 });
