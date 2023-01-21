cd1a96c843bd8b6827566fa4ea765f2ee497dbf3
Sting Alleman <stingalleman@icloud.com>
2023-01-21 15:06:14 +0100

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  outDir: "../dist",
+  // outDir: "../dist",
 });
